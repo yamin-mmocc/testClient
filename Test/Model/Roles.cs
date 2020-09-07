d0ad80394448073efae2a9f135453cc9e4899a3d
yamin-mmocc <yamin.mmocc@gmail.com>
2020-09-07 16:00:21 +0630

    yamin

--- Test/Model/Roles.cs
+++ Test/Model/Roles.cs
@@ -11,5 +11,7 @@ namespace Test.Model
         public bool IsActive { get; set; }
         public DateTime timeStamp { get; set; }
         public long? Role_ID { get; set; }
+
+        public long? Testing_ID { get; set; }
     }
 }
